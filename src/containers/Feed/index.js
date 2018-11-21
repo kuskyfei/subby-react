000dca66cbabc10a2cdd1551cee6a82b54e83daa
Dave <davedmarketing@gmail.com>
2018-11-21 10:14:33 +0700

    remove padding mobile feed

--- src/containers/Feed/index.js
+++ src/containers/Feed/index.js
@@ -39,8 +39,6 @@ const debug = require('debug')('containers:Feed')
 const styles = theme => ({
   layout: {
     width: 'auto',
-    marginLeft: theme.spacing.unit * 2,
-    marginRight: theme.spacing.unit * 2,
     [theme.breakpoints.up(600 + theme.spacing.unit * 2 * 2)]: {
       width: 600,
       marginLeft: 'auto',
